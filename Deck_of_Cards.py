5b225b2ee65c2be5b037e7ebd77fd51a93f58b20
Rommel5055 <javiergonzalez@alumnos.uai.cl>
2021-01-17 01:30:19 +0000

    player

--- Deck_of_Cards.py
+++ Deck_of_Cards.py
@@ -4,6 +4,7 @@ Created on Sun Jan 17 00:55:09 2021
 
 @author: jgonz
 """
+import random
 
 class Card():
     def __init__(self, suit, val):
@@ -26,11 +27,33 @@ class Deck():
     def show(self):
         for c in self.cards:
             c.show()
-    
+            
+    def shuffle(self):
+        for i in range(len(self.cards)-1, 0, -1):
+            r = random.randint(0, i)
+            self.cards[i], self.cards[r] = self.cards[r], self.cards[i]
+            
+    def draw(self):
+        return self.cards.pop()
+        
 class Player():
-    def __init__(self):
-        print("P")
+    def __init__(self, name):
+        self.name = name
+        self.hand = []
+        
+    def draw(self, deck):
+        self.hand.append(deck.draw())
+        
+    def showHand(self):
+        for card in self.hand:
+            card.show()
         
 
 deck = Deck()
-deck.show()
+deck.shuffle()
+
+
+john = Player("John")
+john.draw(deck)
+john.draw(deck)
+john.showHand()
